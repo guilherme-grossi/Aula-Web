0e1e59de6d65f766d21ba8bf6d29df7d2c071f63
guilherme-grossi <guilhermegrossi1000@gmail.com>
2022-01-26 21:08:51 -0300

    Criando Rota

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/usuarios", (request, response) => {
+    response.json({ usuarios: [] })
+});
+
+app.get("/produtos", (request, response) => {
+    response.json({ produtos: [] })
+});
+
 app.listen(3000, () => {
     console.log("servidor iniciado")
-})
+});
