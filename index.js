bc467d8c593672b7c606cd3cc0e879c6e6831d0d
guilherme-grossi <guilhermegrossi1000@gmail.com>
2022-01-26 21:58:57 -0300

    Testando Post

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const express = require("express");
 const app = express();
 
 app.get("/usuarios", (request, response) => {
-    response.json({ usuarios: [] })
+    response.json({ usuarios: usuario })
 });
 
 app.get("/produtos", (request, response) => {
     response.json({ produtos: [] })
 });
+
+app.post('/usuarios', (request, response) => {
+const usuario = {
+    nome: "Guilherme",
+    sobrenome: "Grossi",
+}
+usuarios.push(usuario);
+
+response.json({ usuario });
+})
 
 app.listen(3000, () => {
     console.log("servidor iniciado")
